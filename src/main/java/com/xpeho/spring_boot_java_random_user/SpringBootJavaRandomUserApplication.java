1df12ec5568cd5462d797211af3440c79ed30808
Martho Evan <martho.evan@gmail.com>
2026-02-27 14:57:35 +0100

    feat(docker): add docker with postgresql

--- src/main/java/com/xpeho/spring_boot_java_random_user/SpringBootJavaRandomUserApplication.java
+++ src/main/java/com/xpeho/spring_boot_java_random_user/SpringBootJavaRandomUserApplication.java
@@ -1,5 +1,6 @@
 package com.xpeho.spring_boot_java_random_user;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootJavaRandomUserApplication {
 
 	public static void main(String[] args) {
+		Dotenv.configure()
+				.ignoreIfMissing()
+				.load()
+				.entries()
+				.forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(SpringBootJavaRandomUserApplication.class, args);
 	}
 
