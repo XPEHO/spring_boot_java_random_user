cc2b25b261aecafce8becef0467ad5f54398f819
Martho Evan <martho.evan@gmail.com>
2026-04-16 14:38:43 +0200

    fix(all-tests): change the configuration and add information in .env

--- src/test/java/com/xpeho/spring_boot_java_random_user/presentation/UserGetByIdContainerTest.java
+++ src/test/java/com/xpeho/spring_boot_java_random_user/presentation/UserGetByIdContainerTest.java
@@ -13,8 +13,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.resttestclient.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.ActiveProfiles;
-import org.testcontainers.containers.PostgreSQLContainer;
+
+import org.testcontainers.postgresql.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
@@ -22,7 +22,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @Testcontainers
 @AutoConfigureTestRestTemplate
-@ActiveProfiles("test")
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
         properties = {
@@ -34,7 +33,7 @@ class UserGetByIdContainerTest {
 
     @Container
     @ServiceConnection
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
+    static PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:17-alpine");
 
     @Autowired
     private TestRestTemplate restTemplate;
